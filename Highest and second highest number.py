793737fc0e8d4d926fd9fc21342e83531a802f76
Dhriti <168338013+dhriti-j@users.noreply.github.com>
2024-11-15 15:28:33 +0530

    Update and rename program 12.py to Highest and second highest number.py

--- Highest and second highest number.py
+++ Highest and second highest number.py
@@ -5,7 +5,7 @@ def high(list1):
         list1= sorted(list1)
         print("The highest number is",list1[len(list1)-1], "and the second highest number is",list1[len(list1)-2])
   
-
+#Assuming all numbers added are unique
 list1 = []
 n= int(input("Enter the number of integers to be added to list: "))
 for i in range(n):
